f62e6c85a1dd857eba146c7892689ee6a5be326a
Joban <jobansandhu418@gmail.com>
2019-12-15 10:54:49 +1300

    Add project files.

---
+++ VideoRental/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace VideoRental
+{
+    public class DataBase
+    {
+        //global declaration of the variable 
+        public SqlConnection connection;
+        public String connection_String = System.Configuration.ConfigurationManager.ConnectionStrings["Conn_String"].ConnectionString;
+        public SqlCommand command;
+        public SqlDataReader Datareader;
+    }
+}
